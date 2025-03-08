1e07cffa7aef79d3921b11b5e01e5a8256c15280
hariwebchirpy <hari@webchirpy.com>
2025-03-08 16:48:03 +0530

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react(),
   tailwindcss(),
-]
+],
+build: {
+  outDir: 'dist', // Ensure this is correct
+},
 })
